c88dbb20f795d55128c51ed8927382255e9ac7eb
David Allysson <davidmoreirainformatica@gmail.com>
2017-11-05 11:12:43 -0300

    atualização no javascript para responsividade dos posts

--- js/init.js
+++ js/init.js
@@ -3,5 +3,12 @@
 
     $('.button-collapse').sideNav();
 
+    $(window).resize(function() {
+      if ($('html').innerWidth() < 992)
+        $('.card').removeClass('horizontal');
+      else
+        $('.card').addClass('horizontal');
+    });
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
